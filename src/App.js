3b418ada94a66591480da8fc5bd97ba1c8c6f27d
Juna Shrestha <junacodes@gmail.com>
2019-09-28 18:58:33 +0545

    Add my first app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import me from "./juna.jpg"
+
+const App = () => {
+        return (< div className="App" >
+                 <div className="left"><img src={me} alt="juna"/></div>
+                 <p className="heading">Hi I'm Juna</p>
+                 <p className="sub-heading">I'm learning  to code!</p>
+                  <p className="info">Please be in touch for a chat.</p>
+                  <footer className="email">junacodes@gmail.com</footer>
+                < /div >
+                )
+                }
+            
+    
+        export default App;
