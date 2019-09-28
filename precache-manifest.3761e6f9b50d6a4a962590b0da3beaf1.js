d65aa130d4d1e6b0a0f1753313420e9b003d7e5a
Juna Shrestha <junacodes@gmail.com>
2019-09-28 19:02:31 +0545

    Updates

---
+++ precache-manifest.3761e6f9b50d6a4a962590b0da3beaf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d520b37ce917eb36894a5e3e7fc81f91",
+    "url": "/juns/index.html"
+  },
+  {
+    "revision": "aec6c71131e3f2ed21e5",
+    "url": "/juns/static/css/main.a2683a6d.chunk.css"
+  },
+  {
+    "revision": "3170badc027330bbbc30",
+    "url": "/juns/static/js/2.4aadf3b6.chunk.js"
+  },
+  {
+    "revision": "aec6c71131e3f2ed21e5",
+    "url": "/juns/static/js/main.03bad36f.chunk.js"
+  },
+  {
+    "revision": "7c44e77a635cb6c731f1",
+    "url": "/juns/static/js/runtime-main.68b0e341.js"
+  },
+  {
+    "revision": "d139d17664ac67e94e747a15fc1b8ccb",
+    "url": "/juns/static/media/juna.d139d176.jpg"
+  }
+]);
